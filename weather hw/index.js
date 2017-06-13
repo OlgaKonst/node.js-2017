51464e37cc2acdc19c0b5ee12a96575cc0132d5a
OlgaKonst <olhakonst@gmail.com>
2017-06-13 16:18:49 +0300

    add date

--- weather hw/index.js
+++ weather hw/index.js
@@ -2,9 +2,10 @@
 const btn = document.querySelector('.btn');
 const weatherBlock = document.querySelector('.weather-forecast-item');
 const picture = document.querySelector('img');
-const tempTitle = document.querySelector('.temprat-title');
-const windTitle = document.querySelector('.wind-title');
-const pressureTitle = document.querySelector('.pressure-title');
+const tempTitle = document.querySelector('.temprat-title>span');
+const windTitle = document.querySelector('.wind-title>span');
+const pressureTitle = document.querySelector('.pressure-title>span');
+const dateSpan = document.querySelector('h2>span');
 
 const url = 'http://api.openweathermap.org/data/2.5/weather?q=Kharkiv,ua&APPID=88ec8d7c9f65bdeb88f6ba35f1766831';
 const imgUrl = 'http://openweathermap.org/img/w/';
@@ -20,7 +21,6 @@ const createAjaxPromise = (method, url, data) => {
                     return reject(ajaxRequest);
                 }
                 const response = JSON.parse(ajaxRequest.responseText);
-                console.log('response',response);
                 return resolve(response);
             }
         });
@@ -28,9 +28,17 @@ const createAjaxPromise = (method, url, data) => {
 }
 function setTimePeriod(data) {
     return new Promise((res,rej) => {
-        setTimeout(() => { console.log('3000ms'); return res(data)}, 3000);
+        setTimeout(() => { console.log('1000ms'); return res(data)}, 1000);
     });
 }
+
+const dateToday = new Date();
+const options = {
+    month: 'long',
+    day: 'numeric'
+}
+dateSpan.textContent =  dateToday.toLocaleString("ru", options);
+
 btn.addEventListener('click', (e) => {
     e.preventDefault();
     setTimePeriod().then(() => {
@@ -40,13 +48,14 @@ btn.addEventListener('click', (e) => {
         return setTimePeriod(data);
     }).then((data) => {
         console.log('out');
-        console.log(data.main.pressure);
-        weatherBlock.style.display = "block";
+        //weatherBlock.style.display = "block";
+        weatherBlock.classList.add("flex-block");
         tempTitle.textContent = data.main.temp;
         windTitle.textContent = data.wind.speed;
         pressureTitle.textContent = data.main.pressure;
         const pictureUrl = imgUrl + data.weather[0].icon;
         picture.setAttribute('src', `${pictureUrl}.png`);
+        picture.setAttribute('alt', data.weather[0].description);
     }).catch((err) => {
         console.log('ajax request failed! ', err);
         });
